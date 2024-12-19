0360a5c9f233c172fa5945f17e9e106ab04e5991
Shivansh Goel <92398906+Shivansh-Goel1078@users.noreply.github.com>
2024-12-19 11:17:19 +0530

    Update dbserver.js

--- server/dbserver.js
+++ server/dbserver.js
@@ -1,14 +1,5 @@
 // Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyB9zbpJ6J8Q5h9I4_YCM15bVU2oa22KUuk",
-    authDomain: "hackathon40-e51b6.firebaseapp.com",
-    databaseURL: "https://hackathon40-e51b6-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "hackathon40-e51b6",
-    storageBucket: "hackathon40-e51b6.appspot.com",
-    messagingSenderId: "723966378859",
-    appId: "1:723966378859:web:7754889868fde2160f422e",
-    measurementId: "G-RV5VBECN2Q"
-  };
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
